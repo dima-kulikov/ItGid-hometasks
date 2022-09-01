b5a723723e4a1565cf4da993ff7de4ec41ff41a1
Dima <skaizy44@gmail.com>
2022-09-01 23:43:36 +0300

    save if/else and switch/case

--- itGit-course-JavaScript v.2.0/unit_03/unit_03.js
+++ itGit-course-JavaScript v.2.0/unit_03/unit_03.js
@@ -68,8 +68,18 @@ document.querySelector('.b-4').onclick = f4;
 // На странице есть input c классом i-5, куда пользователь может ввести число. Есть кнопка b-5 которая запускает функцию f5. Функция должна вывести в .out-5 символ 'm' если число меньше нуля, 0 если число равно нулю и число 1 если больше.
 
 function f5(){
+let input5 = Number(document.querySelector(".i-5").value);
+let out5 = document.querySelector(".out-5");
 
+if(input5 < 0) {
+out5.innerHTML = "m";
 }
+else if (input5 == 0) {
+   out5.innerHTML = 0;
+}else {
+   out5.innerHTML = 1;
+};
+};
 
 document.querySelector('.b-5').onclick = f5;
 
@@ -78,8 +88,15 @@ document.querySelector('.b-5').onclick = f5;
 // На странице есть input с классом i-6, куда пользователь может ввести число. Есть кнопка .b-6 которая запускает функцию f6. Функция должна вывести в  .out-6  слово even если число четное и odd если нечетное. Для проверки четности используется целочисленный остаток от деления на 2 (оператор %). Если остаток равен нулю  - четное, нет - нечетное.
 
 function f6(){
+let input6 = Number(document.querySelector('.i-6').value);
+let out6 = document.querySelector(".out-6");
 
-}
+if(input6 % 2 == 0) {
+   out6.innerHTML = "even";
+} else {
+   out6.innerHTML ="odd";
+};
+};
 
 document.querySelector('.b-6').onclick = f6;
 
@@ -87,7 +104,10 @@ document.querySelector('.b-6').onclick = f6;
 // Даны 2 input - .i-71 и .i-72, оба - input[type=number]. При нажатии кнопки .b-7 срабатывает функция f7. Функция должна число из .i-71 возвести в степень .i-72, вывести результат в  .out-7. Для возведения в степень можно использовать **, или Math.pow.
 
 function f7(){
+let input71 = document.querySelector(".i-71").value;
+let input72 = document.querySelector(".i-72").value;
 
+document.querySelector(".out-7").innerHTML = Math.pow(input71, input72);
 }
 
 document.querySelector('.b-7').onclick = f7;
@@ -96,7 +116,17 @@ document.querySelector('.b-7').onclick = f7;
 // Дан select s-8, который содержит 3 значения: 1, 2, 3. Дана кнопка b-8. При ее  нажатии срабатывает функция f8. Функция должна получить выбранное в select число, потом с помощью switch case сравнить его поочередно с 1, 2, 3. И если число выбрано - 1, то вывести в .out-8 строку one, если 2 - two, если 3 - three. Напоминаю - это программирование. Как указано в задании - так и выводим. Т.е. Three с большой буквы - ошибка!!!
 
 function f8(){
+let select = document.querySelector(".s-8").value;
+let out = document.querySelector(".out-8");
 
+switch (select) {
+   case '1': out.innerHTML = "one"
+   break;
+   case '2': out.innerHTML = "two"
+   break;
+   case '3': out.innerHTML = "three"
+   break;
+}
 }
 
 document.querySelector('.b-8').onclick = f8;
@@ -109,7 +139,20 @@ document.querySelector('.b-8').onclick = f8;
 //  В противном случае, вывести 0.
 
 function f9(){
+let input9 = document.querySelector(".i-9").value;
+let out9 = document.querySelector(".out-9");
 
+if(input9 >= 1 && input9 <= 32){
+   out9.innerHTML = 1;
+}
+else if(input9 >= 33 && input9 <= 43){
+   out9.innerHTML = 2;
+}
+else if(input9 >= 44 && input9 <= 64){
+   out9.innerHTML = 3;
+}else {
+   out9.innerHTML = 0;
+}
 }
 
 document.querySelector('.b-9').onclick = f9;
@@ -118,8 +161,9 @@ document.querySelector('.b-9').onclick = f9;
 // Дан select .s-100. По нажатию кнопки, выведите value выбранного option в .out-10.
 
 function f10(){
-
-}
+let select = document.querySelector(".s-100");
+document.querySelector(".out-10").innerHTML = select.value;
+};
 
 document.querySelector('.b-10').onclick = f10;
 
@@ -127,7 +171,8 @@ document.querySelector('.b-10').onclick = f10;
 // Дан select .s-110. По изменению состояния select (событие onchange) выведите value выбранного option в .out-11.
 
 function f11(){
-
+let select = document.querySelector('.s-110').value;
+document.querySelector('.out-11').innerHTML = select;
 }
 
 document.querySelector('.s-110').onchange = f11;
@@ -138,9 +183,8 @@ document.querySelector('.s-110').onchange = f11;
 let i120 = document.querySelector('.i-120');
 
 function f12(){
-   // let v = i120.value;
-   // document.querySelector('.out-12').innerHTML = (typeof v);
-
+let v = i120.value;
+   document.querySelector('.out-12').innerHTML = (typeof v);
 }
 
 document.querySelector('.b-12').onclick = f12;
@@ -149,7 +193,8 @@ document.querySelector('.b-12').onclick = f12;
 // Дан input i-130. В отличие от предыдущего задания - input type number. По нажатию кнопки получите значение из input  в переменную, а затем выведите в out-13 typeof полученной переменной. Typeof позволяет определить тип данных. Если вы правильно все сделали - то удивительно, но тип данных будет string! Подумайте почему так?
 
 function f13(){
-
+let input130 = document.querySelector(".i-130").value;
+document.querySelector('.out-13').innerHTML = (typeof input130);
 }
 
 document.querySelector('.b-13').onclick = f13;
@@ -159,7 +204,21 @@ document.querySelector('.b-13').onclick = f13;
 // Дан input .i-141 и .i-142, type=number.  Дан select .s-143, который содержит две операции - +, -, *, / . Дана кнопка b-14, при нажатии на которую срабатывает функция f14. Функция выводит в .out-14 результат операций выбранной в 3-м select к числам введенным в первом и втором input. Например выбрано 1 13 +, нужно вывести результат операции 1+13 т.е.  14.
 
 function f14(){
+let inputFirst = Number(document.querySelector(".i-141").value);
+let inputSecond = Number(document.querySelector(".i-142").value);
+let select = document.querySelector(".s-143").value;
+let answer = document.querySelector(".out-14");
 
+switch (select) {
+   case '+': answer.innerHTML = inputFirst + inputSecond;
+   break;
+   case '-': answer.innerHTML = inputFirst - inputSecond;
+   break;
+   case '*': answer.innerHTML = inputFirst * inputSecond;
+   break;
+   case '/': answer.innerHTML = inputFirst / inputSecond;
+   break;
+};
 }
 
 document.querySelector('.b-14').onclick = f14;
@@ -173,6 +232,7 @@ function f15(){
 }
 
 document.querySelector('.b-15').onclick = f15;
+
 
 
 
