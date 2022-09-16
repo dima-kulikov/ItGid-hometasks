285f90bf59ad0d2ed58c33f2f9c0819b96c985cb
Dima <skaizy44@gmail.com>
2022-09-16 22:58:55 +0300

    vue2

--- vue2-gosha-dudar/lesson-1/js/lesson-1.js
+++ vue2-gosha-dudar/lesson-1/js/lesson-1.js
@@ -1,6 +1,12 @@
 new Vue ({
     el: '#app',
     data: {
-        title: "Hello world!"
+        title: "Hello world!",
+        styleColor:''
+    },
+    methods: {
+        renameText () {
+            this.title = 'rabotaet'
+        }
     }
 });
