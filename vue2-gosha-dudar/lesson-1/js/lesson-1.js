f6814795909fb8d0fa20659d0d3d7c8748f9b632
Dima <skaizy44@gmail.com>
2022-09-18 22:50:52 +0300

    vue2 learn

--- vue2-gosha-dudar/lesson-1/js/lesson-1.js
+++ vue2-gosha-dudar/lesson-1/js/lesson-1.js
@@ -2,11 +2,22 @@ new Vue ({
     el: '#app',
     data: {
         title: "Hello world!",
-        styleColor:''
+        styleColor:'',
+        firstNum: 1,
+        secondNum: 2,
     },
     methods: {
         renameText () {
             this.title = 'rabotaet'
+        },
+        myFunc(e) {
+            this.firstNum = e;
+            this.secondNum = this.firstNum * this.firstNum;
+        }
+    },
+    computed: {
+        threedNum () {
+           return this.firstNum
         }
     }
 });
