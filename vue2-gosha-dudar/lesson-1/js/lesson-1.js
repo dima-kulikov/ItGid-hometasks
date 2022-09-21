737892ff4f51c2d0354ce6024209be3e5adb0f65
Dima <skaizy44@gmail.com>
2022-09-21 18:41:18 +0300

    как
    кака

--- vue2-gosha-dudar/lesson-1/js/lesson-1.js
+++ vue2-gosha-dudar/lesson-1/js/lesson-1.js
@@ -1,3 +1,39 @@
+Vue.component('dima-car', {
+    data: function () {
+        return {
+            mycars: [
+                {name:'Mers'},
+                {name:'Volvo'},
+                {name:'Ferari'},
+                {name:'BMV'},
+                {name:'Subaru'}
+            ]
+        }
+    },
+    template: `<div><div v-for='car in mycars'><p> Марка: {{car.name}}</p></div></div>`
+}),
+
+
+new Vue ({
+    el:"#dima"
+})
+
+
+
+Vue.component ('vue-car', {
+    data: function () {
+        return {
+            cars: [
+                {name: 'Dima', age: 35 },
+                {name: 'Lesia', age: 27 },
+                {name: 'Kuma', age: 2 },
+                {name: 'kaka' , age: 17 },
+            ] 
+        }
+    },
+    template: `<div><div v-for='car in cars'><p>{{car.name}}</p></div></div>`
+});
+
 new Vue ({
     el: '#app',
     data: {
