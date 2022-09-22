cb0e8b378373f23ff2fc2c5c3c1b93374cdbcecd
Dima <skaizy44@gmail.com>
2022-09-22 21:14:03 +0300

    les-10

--- itGit-course-JavaScript v.2.0/unit_09/unit_09.js
+++ itGit-course-JavaScript v.2.0/unit_09/unit_09.js
@@ -3,7 +3,9 @@
 
 
 function f1() {
-
+let out1 = document.querySelector('.out-1');
+    out1.style.width = '200px';
+    out1.style.height = '90px';
 }
 
 document.querySelector('.b-1').onclick = f1;
@@ -12,7 +14,7 @@ document.querySelector('.b-1').onclick = f1;
 // По нажатию на кнопку .b-2 функция f2, которая присваивает блоку .out-2 класс .bg-orange.
 
 function f2() {
-
+document.querySelector('.out-2').classList.add('bg-orange');
 }
 
 document.querySelector('.b-2').onclick = f2;
@@ -22,7 +24,7 @@ document.querySelector('.b-2').onclick = f2;
 // По нажатию кнопки .b-3 запускайте функцию f3, которая удаляет у блока .out-3 класс .bg-orange.
 
 function f3() {
-
+document.querySelector('.out-3').classList.remove('bg-orange');
 }
 
 document.querySelector('.b-3').onclick = f3;
@@ -32,7 +34,7 @@ document.querySelector('.b-3').onclick = f3;
 
 
 function f4() {
-
+    document.querySelector('.out-4').classList.toggle('bg-orange');
 }
 
 document.querySelector('.b-4').onclick = f4;
