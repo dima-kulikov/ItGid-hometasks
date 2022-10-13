64e85f4b72ac053770e6d9af263c992cd70af586
Dima <skaizy44@gmail.com>
2022-10-13 18:21:12 +0300

    нерн
    нтон

--- itGit-course-JavaScript v.2.0/in-work/unit_18/unit_18.js
+++ itGit-course-JavaScript v.2.0/in-work/unit_18/unit_18.js
@@ -19,7 +19,13 @@ document.querySelector('.b-1').onclick = t1;
 запускаться при вызове функции t2.*/
 
 function t2() {
-
+    let a1 = [2,3,4,5,10,11,12];
+    a1.forEach( el => {
+        if(el % 2 == 0){
+            a2_res.push(el);
+        };
+    });
+    document.querySelector('.out-2').innerHTML = a2_res;
 }
 
 document.querySelector('.b-2').onclick = t2;
@@ -29,7 +35,14 @@ document.querySelector('.b-2').onclick = t2;
 /*  Дан массив a3 = [2, 'hello', 3, 'hi', 4, 'Mazai'] - с помощью forEach переберите массив и создайте новый массив a3_res куда добавьте элементы данного массива являющиеся числом. Запускаться решение должно при вызове функции t3.*/
 
 function t3() {
-
+let a3 = [2, 'hello', 3, 'hi', 4, 'Mazai'] ;
+    
+    a3.forEach( el => {
+        if(typeof el == "number") {
+            a3_res.push(el);
+        };
+    });
+    document.querySelector('.out-3').innerHTML = a3_res;
 }
 
 document.querySelector('.b-3').onclick = t3;
@@ -39,7 +52,12 @@ document.querySelector('.b-3').onclick = t3;
 /*  На странице созданы 3 span.task-4 c атрибутом data. С помощью forEach переберите их и добавьте атрибуты в массив a4_res  Запускаться решение должно при вызове функции t4.*/
 
 function t4() {
+let spanAll = document.querySelectorAll('.task-4');
 
+    spanAll.forEach( el => {
+        a4_res.push(el.getAttribute('data'));
+    })
+    document.querySelector('.out-4').innerHTML = a4_res;
 }
 
 document.querySelector('.b-4').onclick = t4;
@@ -49,7 +67,8 @@ document.querySelector('.b-4').onclick = t4;
 /*  На странице созданы 3 p.task-5 c атрибутом data. С помощью forEach переберите их и добавьте событие клик. Напишите функцию t5, которая будет запускаться при клике и добавлять атрибут data элемента, по которому кликнули в массив a5_res.*/
 
 function t5() {
-
+    a5_res.push(this.getAttribute('data'));
+    document.querySelector('.out-5').innerHTML = a5_res;
 }
 
 document.querySelectorAll('.task-5').forEach(elem => {
@@ -61,8 +80,11 @@ document.querySelectorAll('.task-5').forEach(elem => {
 // Task 6 ============================================
 /*  Дана строка str6='helloworld' - преобразуйте ее в массив и присвойте a6_res. Выведите на страницу. Запускаться решение должно при вызове функции t6. */
 
-function t6() {
+let str6 = 'helloworld';
 
+function t6() {
+    a6_res.push(str6);
+    document.querySelector('.out-6').innerHTML = a6_res;
 }
 
 document.querySelector('.b-6').onclick = t6;
@@ -71,8 +93,13 @@ document.querySelector('.b-6').onclick = t6;
 // Task 7 ============================================
 /*  Дана строка str7='hello world hi mazai' - преобразуйте ее в массив и разбейте по словам. Причем слова не должны содержать пробелов и присвойте a6_res. выведите на страницу.  Запускаться решение должно при вызове функции t7. */
 
+let str7 = 'hello world hi mazai';
 function t7() {
 
+    str7.split(' ').forEach(el => {
+        a7_res.push(el)
+    })
+   document.querySelector('.out-7').innerHTML = a7_res;
 }
 
 document.querySelector('.b-7').onclick = t7;
@@ -80,8 +107,12 @@ document.querySelector('.b-7').onclick = t7;
 // Task 8 ============================================
 /*  Дан массив a8 = [1,2,66,77,15] - преобразуйте ее в строку. Разделитель - дефис. Результат присвойте a8_res. Запускаться решение должно при вызове функции t8. */
 
-function t8() {
+let a8 = [1,2,66,77,15];
 
+function t8() {
+    a8_res = a8.join('-');
+    
+    document.querySelector('.out-8').innerHTML = a8_res;
 }
 
 document.querySelector('.b-8').onclick = t8;
@@ -90,8 +121,16 @@ document.querySelector('.b-8').onclick = t8;
 // Task 9 ============================================
 /*  Дан массив a9 = [[hi, mahai], [test, best]] - преобразуйте его в строку. Разделитель - дефис. Результат присвойте a9_res. Запускаться решение должно при вызове функции t9. Допускается лишний дефис в конце строки!!! */
 
-function t9() {
+let a9 = [['hi', 'mahai'], ['test', 'best']];
 
+function t9() {
+ a9.forEach( el => {
+    el.forEach( elem => {
+        a9_res += elem + '-';
+    });
+ });
+ a9_res =  a9_res.slice(0, -1);
+ document.querySelector('.out-9').innerHTML = a9_res;
 }
 
 document.querySelector('.b-9').onclick = t9;
@@ -99,9 +138,20 @@ document.querySelector('.b-9').onclick = t9;
 
 // Task 10 ============================================
 /*  Дан массив a10 = {name: ivan, age: 15, sex: 1, id: 45} - преобразуйте его в query строку ( так передаются GET параметры). Найдите описание что такое query строка самостоятельно. Разделитель - амперсанд. Результат присвойте a10_res. Запускаться решение должно при вызове функции t10. Допускается лишний амперсанд в конце строки!!! */
-
+let a10 = {
+    name: 'ivan',
+     age: 15,
+      sex: 1,
+       id: 45
+    }
 function t10() {
-
+    a10_res = '?'
+    for(let key in a10){
+        a10_res += `${key}=${a10[key]}&`;
+    };
+    a10_res =  a10_res.slice(0, -1);
+    document.querySelector('.out-10').innerHTML = a10_res;
 }
 
 document.querySelector('.b-10').onclick = t10;
+
