bad621ccd43f4bf0fe2b9b587abd7ec0cf166b5e
Dima <skaizy44@gmail.com>
2022-10-18 18:16:20 +0300

    sad
    sds

--- itGit-course-JavaScript v.2.0/in-work/unit_20/unit_20.js
+++ itGit-course-JavaScript v.2.0/in-work/unit_20/unit_20.js
@@ -67,14 +67,12 @@ document.querySelector('.i-5').onkeydown = t5;
 let out6 = '';
 
 function t6() {
-    console.log(event);
-
 if(event.key === event.key.toLowerCase()){
     out6 += event.key;
     document.querySelector('.i-6').value = out6;
-}else{
-    return false;
 }
+    return false;
+
 }
 
 document.querySelector('.i-6').onkeypress= t6;
@@ -86,7 +84,8 @@ document.querySelector('.i-6').onkeypress= t6;
 const a7 = ['a', 'z', 'x', 'w', 'y', 't'];
 
 function t7() {
-
+    let rand = Math.floor(Math.random() * a7.length);
+    document.querySelector('.out-7').innerHTML = a7[rand];
 }
 
 document.querySelector('.i-7').onkeydown= t7;
@@ -99,8 +98,18 @@ const a8 = {
     o : 0,
     l : 7
 }
-
+ let answer = ''
 function t8(event) {
+    let inputValue = document.querySelector('.i-8').value;
+
+    if(a8[event.key] !== undefined){
+        answer += a8[event.key];
+        document.querySelector('.out-8').innerHTML = answer;
+        
+    }else {
+        answer += event.key;
+        document.querySelector('.out-8').innerHTML = answer;
+    }
     // 1. Получаем из event введенный символ
     // 2. Проверяем наличие такого ключа в a8 - уже делали это раньше!!!!
     // 3. Если есть дописываем в out-8 символ из массива a8. Если нет - введенный символ.
@@ -111,9 +120,14 @@ document.querySelector('.i-8').onkeydown= t8;
 
 // Task 9 ============================================
 /* Дан input .i-9. Напишите функцию t9, выводит в .out-9 количество (число) нажатых клавиш стрелка вниз. */
-
+let start = 1;
 function t9(event) {
     console.log(event);
+
+    if(event.key == 'ArrowDown'){
+        document.querySelector('.out-9').innerHTML = start;
+        start += 1;
+    }
 }
 
 document.querySelector('.i-9').onkeydown = t9;
@@ -126,9 +140,18 @@ let h = 75;
 let w = 75;
 
 function t10(event) {
-    // увеличиваем  h, w, потом присваиваем как свойства...
+    console.log(event);
+    let block = document.querySelector('.block-10');
 
-}
+    // увеличиваем  h, w, потом присваиваем как свойства...
+    if(event.key == 'ArrowDown' || event.key == 'ArrowUp'){
+        h += 1;
+       block.style.height = h + 'px';
+    }else if(event.key == 'ArrowRight' || event.key == 'ArrowLeft'){
+        w += 1;
+        block.style.width = w + 'px';
+    };
+};
 
 document.querySelector('.i-10').onkeydown = t10;
 
@@ -140,11 +163,30 @@ document.querySelector('.i-10').onkeydown = t10;
 4. Самостоятельно добавьте все цифры и второй ряд клавиш от a до l
 5. Самостоятельно добавьте клавишу alt, enter.
 */
-
+let task11 = {
+    Control: 'ctrl',
+    ' ': 'space',
+}
 function t11(event) {
     console.log(event.key);
+    let allKeyboards = document.querySelectorAll('.keyboard');
 
+// удаление класса у всех элементов
+    for(let i = 0; i < allKeyboards.length; i++) {
+        allKeyboards[i].classList.remove('active');
+    };
+
+//поиск нужного ключа в созданном объекте, если есть, то подставляем свои значения и класс
+if(task11[event.key] !== undefined) {
+    let task11Key = task11[event.key];
+    let myBtn = document.querySelector(`.keyboard[data="${task11Key}"]`);
+    myBtn.classList.add('active');
 }
-
+// поиск и добавление нужному элементу
+    for(let i = 0; i < allKeyboards.length; i++) {
+        if(allKeyboards[i].getAttribute('data') == event.key) {
+            allKeyboards[i].classList.add('active');
+        }
+}
+}
 document.querySelector('.i-11').onkeydown = t11;
-
