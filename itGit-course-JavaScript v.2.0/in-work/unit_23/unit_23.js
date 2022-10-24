e50061ea180a468aa0f8504603bc3e3d04bf4d82
Dima <skaizy44@gmail.com>
2022-10-24 16:38:54 +0300

    grg
    rgr

--- itGit-course-JavaScript v.2.0/in-work/unit_23/unit_23.js
+++ itGit-course-JavaScript v.2.0/in-work/unit_23/unit_23.js
@@ -12,66 +12,85 @@ document.querySelector('.b-1').onclick = t1;
 /* Создайте функцию t2 которая записывает  в LS  массив a2 = [7,6,5]. Ключ a2. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-2. */
 
 function t2() {
-
+localStorage.setItem('a2', JSON.stringify([7,6,5]));
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-2').onclick = t2;
 
 // Task 3 ============================================
 /*  При нажатии кнопки t3 выведите из LS сохранненный массив a2. Выведите в out-3 в формате ключ пробел значение перенос строки.  */
 
 function t3() {
-
+let key = localStorage.key(0);
+let value = JSON.parse(localStorage.getItem('a2'));
+document.querySelector('.out-3').innerHTML = `${key} ${value} <br>`
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-3').onclick = t3;
 
 // Task 4 ============================================
 /*  Создайте функцию t4 которая записывает  в LS  массив a4 = {hello: world, hi:mahai}. Ключ a4. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-4.*/
 
 function t4() {
-
+localStorage.setItem('a4', JSON.stringify({hello: 'world', hi:'mahai'}));
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-4').onclick = t4;
 // Task 5 ============================================
 /*   При нажатии кнопки b-5 выведите из LS сохранненный массив a4. Выведите в out-5 в формате ключ пробел значение перенос строки. */
 
 function t5() {
+    let value = JSON.parse(localStorage.getItem('a4'));
+    let allKey = '';
 
+    function myKey() {
+        for(key in value){
+            allKey += `${key} ${value[key]} <br>`;
+        };
+    };
+    myKey();
+    document.querySelector('.out-5').innerHTML = allKey;
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-5').onclick = t5;
 // Task 6 ============================================
 /*  Создайте функцию t6 которая очищает весь LS. Запуск по кнопке b-6*/
 
 function t6() {
-
+localStorage.clear();
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-6').onclick = t6;
 
 // Task 7 ============================================
 /*  Создайте input i-7 куда пользователь может вводить числа и строки. Создайте массив a7. Когда пользователь нажимает кнопку b-7 число должно добавляться в массив. Массив должен сохраняться в LS с ключем a7.*/
+let a7 = [];
 
 function t7() {
+let input = document.querySelector('.i-7').value;
+
+a7.push(input);
+
+localStorage.setItem('a7', JSON.stringify(a7));
 
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-7').onclick = t7;
 // Task 8 ============================================
 /*   Создайте функцию t8 при запуске которой из a7 удаляется последний элемент. После чего массив сохраняется в LS с ключем a7. Использовать массив из предыдущего задания. */
 
 function t8() {
-
+let arr = JSON.parse(localStorage.getItem(('a7')));
+arr.pop()
+localStorage.setItem('a7', JSON.stringify(arr));
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-8').onclick = t8;
 
