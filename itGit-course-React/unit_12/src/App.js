09f7c760d6229011b04e3b95f31dadd9e29969b9
Dima <skaizy44@gmail.com>
2022-12-08 22:30:29 +0200

    вцу
    уву

---
+++ itGit-course-React/unit_12/src/App.js
@@ -0,0 +1,14 @@
+
+import GoodsList from "./containers/GoodsList";
+function App() {
+  return (
+<div>
+      {/* Вывод данных из хранилища */}
+      
+        <GoodsList/>
+
+      </div>
+  );
+}
+
+export default App;
---
+++ itGit-course-React/unit_12/src/containers/GoodsList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import Goods from '../components/Goods';
+import {selectGoods} from '../store/goodsSlice';
+import {increment} from '../store/cartSlice';
+
+
+// Get data from store
+// list data
+
+function GoodsList() {
+    const goods = useSelector(selectGoods);
+    const dispatch = useDispatch();
+
+    let clickHandler = (event) => {
+        event.preventDefault();
+        let t = event.target;
+        if(!t.classList.contains('add-to-cart')) return true;
+        dispatch(increment(t.getAttribute('data-key')))
+
+    }
+
+    return(
+        <>
+            <div className='goods-field' onClick={clickHandler}>
+                {goods.map(item => 
+                <Goods title={item.title} cost={item.cost} image={item.image} articul={item.articul} key={item.articul} />
+                )}
+            </div>
+        </>
+    );
+}
+
+export default GoodsList;
