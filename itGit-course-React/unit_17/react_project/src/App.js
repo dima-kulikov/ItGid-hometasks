c97f434922c6ce9b0e893bbc8f1d3b60a3e0a90b
Dima <skaizy44@gmail.com>
2022-12-13 20:38:03 +0200

    люл
    юлюл

--- itGit-course-React/unit_17/react_project/src/App.js
+++ itGit-course-React/unit_17/react_project/src/App.js
@@ -1,3 +1,4 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import About from "./components/About";
 import Create from "./components/Create";
 import Error from "./components/Error";
@@ -9,7 +10,18 @@ import Note from "./components/Note";
 function App() {
   return (
     <div className="main">
-      
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route exact path='/' element={<Main/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/create' element={<Create/>} />
+          <Route exact path='/note' element={<Note/>} />
+          <Route exact path='/note/:noteURL' element={<Note/>} />
+          <Route element={<Error/>} />
+        </Routes>
+      </Router>
+      <Footer></Footer>
     </div>
   );
 }
