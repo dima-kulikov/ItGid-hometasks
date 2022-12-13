7280f641b5a9198e8a3eb59f1ef5d1a932a0532b
Dima <skaizy44@gmail.com>
2022-12-13 12:54:56 +0200

    и аиа
    иаи

---
+++ itGit-course-React/unit_17/react_project/src/App.js
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Create from "./components/Create";
+import Error from "./components/Error";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Note from "./components/Note";
+
+function App() {
+  return (
+    <div className="main">
+      
+    </div>
+  );
+}
+
+export default App;
